6caedfa51ed36fb7498208c3461fbf5e3058d477
VeRJiL <arash.nykbakht@gmail.com>
2019-10-16 08:48:13 +0330

    Update TelegramBotController.php

--- app/Http/Controllers/TelegramBotController.php
+++ app/Http/Controllers/TelegramBotController.php
@@ -1,12 +1,1 @@
-<?php
 
-$botToken = "asdfsadfsadfsadsdfsdf";
-$chat_id = "@dabatestbot";
-$message = "hi";
-$bot_url    = "https://api.telegram.org/bot$botToken/";
-$url = $bot_url."sendMessage?chat_id=".$chat_id."&text=".urlencode($message);
-echo $url;
-die();
-file_get_contents($url);
-
-?>
